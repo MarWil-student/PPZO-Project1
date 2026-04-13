ef111477e36193cf51d17b87900f20a2219ed67d
MarWil-student <martyna.wilkosz@student.wsb.edu.pl>
2026-04-13 17:44:13 +0200

    done with python calculator

--- Main-python/Main_python.py
+++ Main-python/Main_python.py
@@ -1,1 +1,31 @@
+def calculator():
+    num1 = float(input("Podaj pierwsza liczbe"))
+    num2 = float(input("Podaj druga liczbe"))
+    option = input("Wybierz operację (+, -, *, /): ")
+    if option == "+":
+        print(f"Wynik: {num1 + num2}")        
+    elif option == "-":
+        print(f"Wynik: {num1 - num2}")            
+    elif option == "*":
+        print(f"Wynik: {num1 * num2}")         
+    elif option == "/":
+            if num2 == 0:
+                print("Nie można dzielić przez 0")
+            else:
+                print(f"Wynik: {num1 / num2}")
+    else:
+        print("Zla opcja")
 
+def main():
+    print("Wybierz opcję: 1-kalkulator, 2-konwerter, 3-srednia")
+    option = input()  
+    match option:
+        case "1":
+            calculator()
+        case "2":
+            print("converter")
+        case "3":
+            print("student gpa")
+
+if __name__ == "__main__":
+    main()
