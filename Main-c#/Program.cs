38c04242f13105b60ba6b4ef9c99217d79614aab
MarWil-student <martyna.wilkosz@student.wsb.edu.pl>
2026-04-13 15:17:54 +0200

    Decided to make it in one file with switch

---
+++ Main-c#/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    static void Calculator()
+    {
+        Console.WriteLine("Podaj pierwsza liczbe: ");
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj druga liczbe: ");
+        float num2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wybierz operację (+, -, *, /): ");
+        string option = Console.ReadLine();
+
+        if (option == "+")
+        {
+            Console.WriteLine($"Wynik: {num1 + num2}");
+        }
+        else if (option == "-")
+        {
+            Console.WriteLine($"Wynik: {num1 - num2}");
+        }
+        else if (option == "*")
+        {
+            Console.WriteLine($"Wynik: {num1 * num2}");
+        }
+        else if (option == "/")
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Nie można dzielic przez 0");
+            }
+            else
+            {
+                Console.WriteLine($"Wynik: {num1 * num2}");
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Zla opcja");
+        }
+    }
+    static void Main()
+    {
+        Console.WriteLine("Wybierz opcje: 1-kalkulator, 2-konwerter, 3-srednia");
+        string opt = Console.ReadLine();
+
+        switch (opt)
+        {
+            case "1":
+                Calculator();
+                break;
+            
+            case "2":
+                break;  
+
+            case "3": 
+                break;
+
+            default:
+                Console.WriteLine("Zla opcja");
+                break;
+
+        }
+
+    }
+}
