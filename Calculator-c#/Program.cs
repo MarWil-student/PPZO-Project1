2d770fbdc474f1303d88df1c9c55f47c4faec330
MarWil-student <martyna.wilkosz@student.wsb.edu.pl>
2026-04-13 14:59:49 +0200

    Done with c# calculator

---
+++ Calculator-c#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwsza liczbe: ");
+        float num1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj druga liczbe: ");
+        float num2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wybierz operację (+, -, *, /): ");
+        string option = Console.ReadLine();
+
+        if (option == "+")
+        {
+            Console.WriteLine($"Wynik: {num1 + num2}");
+        }
+        else if (option == "-")
+        {
+            Console.WriteLine($"Wynik: {num1 - num2}");
+        }
+        else if (option == "*")
+        {
+            Console.WriteLine($"Wynik: {num1 * num2}");
+        }
+        else if (option == "/")
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Nie można dzielic przez 0");
+            }
+            else
+            {
+                Console.WriteLine($"Wynik: {num1 * num2}");
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Zla opcja");
+        }
+    }
+}
